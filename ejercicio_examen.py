c801916e168fb1a67ff5030d73d0d08435e2fbe7
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-21 17:13:28 +0100

    cambio nombre

--- ejercicio_examen.py
+++ ejercicio_examen.py
@@ -13,43 +13,36 @@ def euler(f,x,y,h,m):
     return u,v
 
 def f(x,y):
-    return (2-x-y)/(x-y+4)
+    return y-x**2+1
 
 
-x = -2
-y = 0
-h = 0.1
+x = 0
+y = 0.5
+h = 0.025
 m = 20
 u,v = euler(f,x,y,h,m)
 
 
 
 x1=0
-y1=2
-h1=0.1
+y1=0
+h1=0.025
 m1=20
 u1,v1=euler(f,x1,y1,h1,m1)
 
 
-x2=2
-y2=4
-h2=0.1
+x2=0
+y2=-1
+h2=0.025
 m2=20
 u2,v2=euler(f,x2,y2,h2,m2)
-
-x3=-3
-y3=4
-h3=0.1
-m3=20
-u3,v3=euler(f,x3,y3,h3,m3)
 
 
 plt.plot(u,v)
 plt.plot(u1,v1)
 plt.plot(u2,v2)
-plt.plot(u3,v3)
+plt.grid(True)
 plt.show()
-
 
 
 
