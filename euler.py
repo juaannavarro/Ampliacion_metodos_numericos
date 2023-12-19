f760d7055bae77571349244d5cac755e5e5375bf
albabernal03 <ALBABERO@MYUAX.COM>
2023-12-19 23:57:23 +0100

    uno de un ejercicio que acabo de practicar

--- euler.py
+++ euler.py
@@ -16,12 +16,12 @@ def euler(f,x,y,h,n):
 def f(x,y):
     '''Aqui añadimos la EDO'''
     #return (np.exp(x))/((1+np.exp(x))*y)
-    return (1+4*x*y)/(3*x**2)
+    return (x*y)/(x**2 + y**2)
 
 def f_exacta(x):
     '''Aqui añadimos la solucion exacta'''
     #return np.sqrt(2 * np.log(1 + np.exp(x)) + 0.746144)
-    return (-1)/(7*x) -1.8*x**(4/3)
+    return np.exp((x**2-y**2)/2*y**2)
 
 
 
@@ -33,22 +33,22 @@ def error(v, v_aprox):
     return abs(v - v_aprox)
 
 #DATOS
-x_inicial=0.5
-x_final=4
+x_inicial=-1
+x_final=1
 
-x = 0.5
-y = -1 
+x = -1
+y = 1
 n = 100 
 h = (x_final-x_inicial)/n
 u,v = euler(f,x,y,h,n)
 
 print('w_100', v[-1])
 
-#solucion real en y(2)
-print('y(4)=', f_exacta(4)) #es la ultima x del rango
+#solucion real en y(1)
+print('y(1)=', f_exacta(1)) #es la ultima x del rango
 
 #Error
-v_exacta = f_exacta(4)
+v_exacta = f_exacta(1)
 print('Error: ', error(v_exacta,v[-1]))
 
 #Grafica
